c964542a7b93d19a3d015544484230cad0e0d29b
SethMatic <52398222+SethMatic@users.noreply.github.com>
2019-11-11 23:48:57 -0500

    Add files via upload
    added seeds and schema for database

---
+++ seeds.sql
@@ -0,0 +1,57 @@
+USE bamazon;
+
+INSERT INTO department (department_name,over_head_costs) 
+VALUES ('Electronics',10000);
+
+INSERT INTO department (department_name,over_head_costs) 
+VALUES ('Clothing',60000);
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Gameboy', 1, 50, 2000,0);
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Playstation', 1, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Xbox', 1, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Macbook', 1, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Television', 1, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Socks', 2, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Hats', 2, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Shirts', 2, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Pants', 2, 75, 2000,0);
+
+
+INSERT INTO product (product_name, department_id, price,
+stock_quantity, product_sales) 
+VALUES ('Shoes', 2, 75, 2000,0);
+
+
